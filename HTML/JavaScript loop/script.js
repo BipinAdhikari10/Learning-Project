dd1c0272194598d97729b837e9a97c25b3eb1528
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-01 12:08:04 +0545

    JavaScript loop

---
+++ HTML/JavaScript loop/script.js
@@ -0,0 +1,15 @@
+let counter = 1;
+while (counter <= 10);
+{
+  console.log(hello);
+  counter++;
+}
+
+for (let i = 0; i < 10; i++);
+console.log(i);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum += sum;
+  console.log(sum);
+}
