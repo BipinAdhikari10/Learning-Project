ed50280cb646295c13feda88c803b15b66d492d9
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-01 13:36:53 +0545

    JavaScript push

---
+++ HTML/28.JavaScript push/script.js
@@ -0,0 +1,31 @@
+let numbers = [1, 2, 3];
+
+numbers.push(4);
+numbers.push(5, 6, 10);
+console.log(numbers);
+
+let fruits = [];
+fruits.push("apple");
+fruits.push("mango", "banana");
+console.log(fruits);
+
+let arr = ["ktm", "syangja"];
+let returnElement = arr.push("kusma");
+console.log(arr);
+
+let vegetables = ["tomato", "onion", "cucumber"];
+vegetables.push("cabbage");
+console.log(vegetables);
+
+let vehicle = ["car", "bus"];
+let returnedElement = vehicle.push("bike");
+
+console.log(vehicle);
+
+let animal = ["lion", "tiger"];
+animal.push("elephant");
+console.log(animal);
+
+let array = [1, 2, 3];
+array.push(5);
+console.log(array);
