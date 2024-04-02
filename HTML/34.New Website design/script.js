c18f54e2f26fa383035a76cb18118f5e0f3d8d50
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-02 21:36:44 +0545

    Concat

--- HTML/34.New Website design/script.js
+++ HTML/34.New Website design/script.js
@@ -1,5 +1,15 @@
+// Find the sum of first 10 natural numbers:
 let sum = 0;
 for (let i = 0; i <= 10; i++) {
   sum += i;
 }
 console.log(sum);
+
+// Write a function to return  the sum of given numbers:
+
+// splice
+
+let fruits = ["apple", "banana", "pear", "mango", "orange"];
+let removedItems = fruits.splice(2, 1);
+console.log(removedItems);
+console.log(fruits);
