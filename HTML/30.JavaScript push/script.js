cce21818a019a880cb0489475147aa9d8470d0e5
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-02 13:07:28 +0545

    JavaScript push

---
+++ HTML/30.JavaScript push/script.js
@@ -0,0 +1,15 @@
+fruits = ["apple ", "banana"];
+fruits.push("orange");
+console.log(fruits);
+
+let numbers = [5, 10, 15, 20];
+numbers.push(50);
+console.log(numbers);
+
+let city = ["ktm", "pkr"];
+let returnItems = city.push("syangja");
+console.log(city);
+
+let vehicle = ["car", "van"];
+vehicle.push("taxi");
+console.log(vehicle);
