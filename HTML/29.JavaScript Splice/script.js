143d53967a5905f2865e3a1767fbae94fef2fa0e
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-02 12:13:22 +0545

    JavaScript Splice

--- HTML/29.JavaScript Splice/script.js
+++ HTML/29.JavaScript Splice/script.js
@@ -3,3 +3,18 @@ let fruits = ["apple", "banana", "grapes"];
 let removedItems = fruits.splice(1, 2, "pear", "mango");
 console.log(removedItems);
 console.log(fruits);
+
+let vehicle = ["car", "van", "bike", "taxi"];
+let removeItems = vehicle.splice(2, 2);
+console.log(vehicle);
+console.log(removeItems);
+
+let vegetables = ["cabbage", "onion", "tomato", "cauliflower"];
+let removeVegetables = vegetables.splice(1, 2, "brocauli", "pea");
+console.log(removeVegetables);
+console.log(vegetables);
+
+let animals = ["dog", "lion", "tiger"];
+let removeAnimals = animals.splice(1, 2, "deer");
+console.log(removeAnimals);
+console.log(animals);
