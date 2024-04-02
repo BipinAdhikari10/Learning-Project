c241afed428b2af206e46d1839a5b94a55f9517d
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-02 22:39:52 +0545

    Join and split in js

---
+++ HTML/37.Join and split in js/script.js
@@ -0,0 +1,9 @@
+const fruits = ["apple", "banana", "mango"];
+const joinedStrings = fruits.join(",");
+console.log(joinedStrings);
+
+// Split
+
+const sentence = "i am bipin adhikari";
+const words = sentence.split("a");
+console.log(words);
