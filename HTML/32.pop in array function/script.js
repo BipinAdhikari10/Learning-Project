4207e5927ecdd96850504b3ba6858becb826c29a
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-02 13:24:37 +0545

    pop in array function

---
+++ HTML/32.pop in array function/script.js
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana"];
+let removedItems = fruits.pop("banana");
+console.log(fruits);
+console.log(removedItems);
+
+numbers = [4, 5, 6];
+removeItems = numbers.pop(4);
+console.log(numbers);
+console.log(removeItems);
