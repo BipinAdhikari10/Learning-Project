1a13bc7a34b707aa14271764a596c9ad3252321e
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-03 18:10:03 +0545

    Reversed String

--- HTML/40.Website design with for loop array function/script.js
+++ HTML/40.Website design with for loop array function/script.js
@@ -1,5 +1,24 @@
-let sum = 0;
-for (let i = 0; i <= 10; i++) {
-  sum = sum + i;
+// // let sum = 0;
+// // for (let i = 0; i <= 10; i++) {
+// //   sum = sum + i;
+// // }
+// // console.log(sum);
+
+// // Arrow function to calculate the sum of first n natural numbers
+// const sumOfFirstN = (n) => {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// };
+
+// const result = sumOfFirstN(10);
+// console.log("Sum of first 10 natural numbers:", result);
+
+const str = "Hello";
+let reversed = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversed += str[i];
 }
-console.log(sum);
+console.log(reversed);
