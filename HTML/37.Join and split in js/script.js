74fd0576cf3310322aac2e6f988749bbf6aa2ff9
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-03 09:50:54 +0545

    Website design

--- HTML/37.Join and split in js/script.js
+++ HTML/37.Join and split in js/script.js
@@ -1,9 +1,37 @@
-const fruits = ["apple", "banana", "mango"];
-const joinedStrings = fruits.join(",");
-console.log(joinedStrings);
+// const fruits = ["apple", "banana", "mango"];
+// const joinedStrings = fruits.join(",");
+// console.log(joinedStrings);
 
-// Split
+// // Split
 
-const sentence = "i am bipin adhikari";
-const words = sentence.split("a");
-console.log(words);
+// const sentence = "i am bipin adhikari";
+// const words = sentence.split("a");
+// console.log(words);
+
+// function returnSum(num) {
+//   let sum = 0;
+//   for (let i = 0; i < num; i++) {
+//     sum = sum + i;
+//   }
+//   console.log(sum);
+// }
+
+// returnSum(5);
+
+// Arrow function:
+let returnedSum = (num) => {
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+let sum = returnedSum(6);
+console.log(sum);
+
+// Arrow function:
+let addNum = (a, b) => a + b; // When only one line of code.
+
+let addedNum = (a, b) => {
+  return a + b;
+};
