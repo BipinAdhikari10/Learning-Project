f95a1624ed192ea58958e7c87e4e1ba68274df9f
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-03 13:18:06 +0545

    JavaScript program for loop

--- HTML/38.JavaScript program for loop/script.js
+++ HTML/38.JavaScript program for loop/script.js
@@ -1,8 +1,29 @@
+// function sumNum(a, b) {
+//   return a + b;
+// }
+// let num = sumNum(2, 4);
+// console.log(num);
+
+// let Sumnum = (a, b) => a + b;
+// console.log(sumNum(2, 2));
+
+// for (let num = 1; num < 10; num++) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+// }
+
+// arrow function
+
 function sumNum(a, b) {
   return a + b;
 }
-let num = sumNum(2, 4);
-console.log(num);
+let sum = sumNum(4, 5);
+console.log(sum);
 
-let Sumnum = (a, b) => a + b;
-console.log(sumNum(2, 2));
+let addNum = (a, b) => a + b;
+let sumA = addNum(4, 5);
+console.log(sumA);
+
+const sub = (a, b) => a - b;
+console.log(sub(5, 4));
