fb9b23133aa268a7ee296b9c528d3dfd92f9891c
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-04 16:01:09 +0545

    multiple of 3 using array function

--- HTML/41.Array practice queastions/script.js
+++ HTML/41.Array practice queastions/script.js
@@ -1,45 +1,58 @@
-// function addNum(arr) {
+// // function addNum(arr) {
+// //   let sum = 0;
+// //   for (let i = 0; i < arr.length; i++) {
+// //     sum += arr[i];
+// //   }
+// //   return sum;
+// // }
+// // let array = [1, 2, 3];
+// // const result = addNum(array);
+// // console.log(result);
+
+// // function evenNum(arr) {
+// //   let newArray = [];
+// //   for (let i = 0; i < arr.length; i++) {
+// //     if (arr[i] % 2 == 0) {
+// //       newArray.push(arr[i]);
+// //     }
+// //   }
+// //   return newArray;
+// // }
+// // let results = evenNum([1, 2, 3, 4, 5, 6]);
+// // console.log(results);
+
+// function arrayNum(arr) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 5) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
+// }
+// let numbers = [1, 2, 3, 4, 5, 8];
+// let result = arrayNum(numbers);
+// console.log(result);
+
+// function addSum(arr) {
 //   let sum = 0;
 //   for (let i = 0; i < arr.length; i++) {
 //     sum += arr[i];
 //   }
 //   return sum;
 // }
-// let array = [1, 2, 3];
-// const result = addNum(array);
-// console.log(result);
+// let number = addSum([1, 2, 3]);
+// console.log(number);
 
-// function evenNum(arr) {
-//   let newArray = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] % 2 == 0) {
-//       newArray.push(arr[i]);
-//     }
-//   }
-//   return newArray;
-// }
-// let results = evenNum([1, 2, 3, 4, 5, 6]);
-// console.log(results);
-
-function arrayNum(arr) {
+function multipleOfThree(arr) {
   let newArray = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5) {
+    if (arr[i] % 3 == 0) {
       newArray.push(arr[i]);
     }
   }
   return newArray;
 }
-let numbers = [1, 2, 3, 4, 5, 8];
-let result = arrayNum(numbers);
-console.log(result);
-
-function addSum(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum;
-}
-let number = addSum([1, 2, 3]);
-console.log(number);
+let number = [2, 3, 6, 9, 12, 15, 5];
+let resuult = multipleOfThree(number);
+console.log(resuult);
