604fc45d4fadee52c497593d08534679446ecc9d
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-04 10:05:58 +0545

    Even number using array function

--- HTML/40.Website design with for loop array function/script.js
+++ HTML/40.Website design with for loop array function/script.js
@@ -16,9 +16,21 @@
 // const result = sumOfFirstN(10);
 // console.log("Sum of first 10 natural numbers:", result);
 
-const str = "Hello";
-let reversed = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversed += str[i];
+// const str = "Hello";
+// let reversed = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversed += str[i];
+// }
+// console.log(reversed);
+
+function EvenNum(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 }
-console.log(reversed);
+let result = EvenNum([1, 2, 3, 4, 5, 6]);
+console.log(result);
