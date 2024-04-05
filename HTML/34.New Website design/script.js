588a1edabb7578a79fae641ed07f97ab721e20cb
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-05 13:28:47 +0545

    array function

--- HTML/34.New Website design/script.js
+++ HTML/34.New Website design/script.js
@@ -1,15 +1,71 @@
-// Find the sum of first 10 natural numbers:
-let sum = 0;
-for (let i = 0; i <= 10; i++) {
-  sum += i;
+// // Find the sum of first 10 natural numbers:
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// // Write a function to return  the sum of given numbers:
+
+// // splice
+
+// let fruits = ["apple", "banana", "pear", "mango", "orange"];
+// let removedItems = fruits.splice(2, 1);
+// console.log(removedItems);
+// console.log(fruits);
+
+// function sumArr(arr)
+// {
+//   let sum=0;
+//   for(let i=0;i<arr.length;i++)
+//   {
+//     sum+=arr[i];
+//   }
+//   return sum
+// }
+// let arr=[1,2,3,4]
+// const result=sumArr(arr)
+// console.log(arr)
+console.log("Hello world");
+
+function findElement(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      return true;
+    }
+    return false;
+  }
 }
-console.log(sum);
+let array = ["car", "van", "taxi", "bus"];
+let ele = "jeep";
+// const isElementExist = findElement(array, ele);
+const isElementExist = array.includes(ele);
+if (isElementExist) {
+  console.log("Element exist");
+} else {
+  console.log("Element doesn't exist");
+}
 
-// Write a function to return  the sum of given numbers:
+function removedElement(arr, index) {
+  arr.splice(index, 1);
+  return arr;
+}
+let arr = [1, 2, 3];
+let result = removedElement(arr, 1);
+console.log(result);
 
-// splice
+function printArrayElement(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+let num = [1, 2, 3, 4, 5];
+let num1 = printArrayElement(num);
+console.log(num1);
 
-let fruits = ["apple", "banana", "pear", "mango", "orange"];
-let removedItems = fruits.splice(2, 1);
-console.log(removedItems);
-console.log(fruits);
+function reversedItems(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+reversedItems(["apple", "ball", "cat", "dog"]);
--- HTML/41.Array practice queastions/script.js
+++ HTML/41.Array practice queastions/script.js
@@ -55,4 +55,33 @@ function multipleOfThree(arr) {
 }
 let number = [2, 3, 6, 9, 12, 15, 5];
 let resuult = multipleOfThree(number);
+
 console.log(resuult);
+
+// existing array find odd number
+
+function oddNum(arr) {
+  odd = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      odd.push(arr[i]);
+    }
+  }
+  return odd;
+}
+let num = [1, 2, 3, 4];
+const result = oddNum(num);
+console.log(result);
+
+// average
+
+function averageSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+let numm = averageSum([1, 2, 3, 4, 5]);
+console.log(numm);
+
