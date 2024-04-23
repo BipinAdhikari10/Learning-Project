f0996b9ceb8ae5163bd6b792985396a49297e1be
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-04-23 14:06:44 +0545

    Navigation html css

---
+++ HTML/45.W3s form/script.js
@@ -0,0 +1,25 @@
+// array to string
+
+console.log("hello");
+
+let fruits = ["apple", "banana"];
+let arrFruits = fruits.join("+ ");
+console.log(arrFruits);
+
+// string to array
+
+let introducton = "my name is Bipin Adhikari";
+let myIntro = introducton.split(" ");
+console.log(myIntro);
+
+// array to string vehicle
+
+let vehicle = ["car", "van"];
+let myVehicle = vehicle.join("+");
+console.log(myVehicle);
+
+// string to array
+
+let sentences = "I live in Kathmandu";
+let mySentence = sentences.split(" ");
+console.log(mySentence);
